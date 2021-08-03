8093ca4e077c598fc94c6e1aba1fef78432e460f
Ivan Minin <wow.mjogan.official@yandex.ru>
2021-08-03 12:39:25 +0300

    увеличен лимит запросов для тестирования

--- utils/limiter.js
+++ utils/limiter.js
@@ -2,5 +2,5 @@ const rateLimit = require('express-rate-limit');
 // ограничиваем количество запросов к API в час
 module.exports.limiter = rateLimit({
   windowMs: 15 * 60 * 1000,
-  max: 100,
+  max: 10000,
 });
